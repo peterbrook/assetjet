0d1ddeb2b56e1543b18b81e43bf39838dab4837a
Felix Zumstein <fzumstein@gmail.com>
2012-11-25 20:39:43 +0100

    added flag for Yahoo! Finance call

--- samplequery.py
+++ samplequery.py
@@ -5,17 +5,19 @@ import sqlite3
 
 # SQLite Database
 db = 'Stocks.db'
+updateDatabase = False
 
 # Populate database with some data from Yahoo! Finance
-tickers = ['GOOG', 'MSFT', 'AAPL', 'UBSN.VX']
-database.populate_db(tickers, '2008-1-1', '2012-11-25', dbfilename=db)
+if updateDatabase:
+    tickers = ['GOOG', 'MSFT', 'AAPL', 'UBSN.VX']
+    database.populate_db(tickers, '2008-1-1', '2012-11-25', dbfilename=db)
 
 # Create a DataFrame from a SQL query 
 conn = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
 df = sql.read_frame("""SELECT symbol, date, adjclose FROM stocks""", conn)
 conn.close()
 
-# Pivot DataFrame to have a easy to work time series
+# Pivot DataFrame to work easily with the time series
 dfPivot = df.pivot('date', 'symbol', 'adjclose')
 returns = dfPivot.pct_change()
 returnIndex = (1 + returns).cumprod()

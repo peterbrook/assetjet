f9ac0f59ddc72ce9e8bf9919aadfa454aa2c9a8a
Mel Padden <melpadden@gmail.com>
2012-11-27 22:11:35 +0100

    Figured out the QTDesigner stuff a little better

--- tripping-nemesis/src/program.main.py
+++ tripping-nemesis/src/program.main.py
@@ -5,18 +5,18 @@ from PyQt4 import QtGui, QtCore
 sys.path.append(os.curdir + "\\UI")
 from tripping_nemesis import Ui_TrippingNemesis
 
-class MainForm(QtGui.QMainWindow, Ui_TrippingNemesis):
+class MainForm(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainForm, self).__init__(parent)
+        self.ui = Ui_TrippingNemesis()  
+        self.ui.setupUi(self)        
         self.model = QtGui.QStandardItemModel()
         
 def main():
     app = QtGui.QApplication(sys.argv)
-    form = MainForm()
-    form.height = 1000
-    form.width = 1200
-    form.show()
-    app.exec_()
+    mainForm = MainForm()
+    mainForm.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()

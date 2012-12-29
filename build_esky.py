4379bc79c3a9c9d3c115722d2ca74b022bedbad2
Felix Zumstein <fzumstein@gmail.com>
2012-12-29 21:26:05 +0100

    Revert "First version of esky build/setup script"
    This reverts commit e76b6740fb34218a2ace09bde5f1f98be55cb6dd.

--- build_esky.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-
-# Freeze it
-call('python setup_esky.py bdist_esky')
-
-#call('python setup_esky.py bdist_esky_patch')
--- build_inno_setup.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-# Compile it
-os.system('"C:\Program Files (x86)\Inno Setup 5\iscc" inno_installer_64bit.iss')

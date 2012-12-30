5434220f36fe4c37a42738e294e4b7e9e0f324d9
Felix Zumstein <fzumstein@gmail.com>
2012-12-30 13:39:26 +0100

    Added Esky POC

--- assetjet/main.py
+++ assetjet/main.py
@@ -1,16 +1,29 @@
 """
-    Tripping Nemesis
+    AssetJet
     Program Entry Point
 """
 import os, sys
 from PySide import QtGui, QtCore
-
+import esky
 sys.path.append('..')
-
 from assetjet.controller.main_controller import MainController 
-"""
-    Application entry point
-"""
+
+###
+# TODO: Very quick POC for Esky - needs to be refactored into silent update in
+# a background thread...
+if getattr(sys,"frozen",False):
+    try:
+        frozenapp = esky.Esky(sys.executable,"http://localhost:8000")
+        new_version = frozenapp.find_update()
+        if(new_version != None):
+            frozenapp.auto_update()
+            appexe = esky.util.appexe_from_executable(sys.executable)
+            os.execv(appexe,[appexe] + sys.argv[1:])
+    except Exception, e:
+        print "ERROR UPDATING APP:", e
+    frozenapp.cleanup()   
+###
+
 def main():
     app = QtGui.QApplication(sys.argv)
     main_form = MainController()

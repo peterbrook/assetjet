84ae6b2caa25790eedc16d26988ee835196ff174
Felix Zumstein <fzumstein@gmail.com>
2013-01-02 01:06:25 +0100

    produce also patches by default

--- build_esky.py
+++ build_esky.py
@@ -1,6 +1,4 @@
 from subprocess import call
 
-# Freeze it
-call('python setup_esky.py bdist_esky')
-
-#call('python setup_esky.py bdist_esky_patch')
+# Freeze it and produce patches in case older versions are in the directory
+call('python setup_esky.py bdist_esky_patch')

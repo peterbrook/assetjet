4ccefaf936b57ea8dccc98658f900f0edb3e8e43
Felix Zumstein <fzumstein@gmail.com>
2013-01-04 18:36:33 +0100

    only log if frozen & log msg more explicit

--- app/src/main.py
+++ app/src/main.py
@@ -23,14 +23,20 @@ def main():
     sys.exit()
 
 if __name__ == '__main__':
-    try:
-        log.Debug("Initialising app")
+    if getattr(sys,"frozen",False):
+    # if app is a frozen executable, use logging
+        try:
+            log.Debug("Initialising app")
+            main()
+            sys.exit(0)
+           
+        except Exception, ex:
+            log.Error("Error in application: {0}", str(ex))
+            sys.exit(-1)
+    else:
+    # don't use logging for debugging
         main()
-        sys.exit(0)
-       
-    except Exception, ex:
-        log.Error("Error in application: {0}", ex.message)
-        sys.exit(-1)
+
 
     
     

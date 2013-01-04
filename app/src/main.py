8234abe5c5c9b241527c038126f3867445cf493a
Felix Zumstein <fzumstein@gmail.com>
2013-01-05 00:17:40 +0100

    moved imports to top and replaced setDaemon

--- app/src/main.py
+++ app/src/main.py
@@ -6,14 +6,13 @@
 import os, sys
 from PySide import QtCore, QtGui
 from assetjet.log import log
+from assetjet.controller.main_controller import MainController 
+from assetjet.cfg import cfg
+from assetjet.util import updater
 
 def main():
-    from assetjet.controller.main_controller import MainController 
-    from assetjet.cfg import cfg
-    from assetjet.util import updater
-        
     upd = updater.Updater(cfg.root.UpdateUrl)
-    upd.setDaemon(daemonic=True)
+    upd.daemon = True
     upd.start()
     
     app = QtGui.QApplication(sys.argv)

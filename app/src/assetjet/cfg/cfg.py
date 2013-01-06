9b38390686b5d6623fc68887036dc7a16e1805e3
Felix Zumstein <fzumstein@gmail.com>
2013-01-06 21:46:57 +0100

    Create config file if it not exists with defaults

--- app/src/assetjet/cfg/cfg.py
+++ app/src/assetjet/cfg/cfg.py
@@ -2,6 +2,22 @@
 import ConfigParser, os
 from assetjet.util import util
 
+fileName = 'app.cfg'
+location = os.path.dirname(util.getBaseDir())
+
+# If config file doesn't exist, create with defaults
+# TODO: get db location from user input upon first startup
+if not os.path.exists(os.path.join(location, fileName)):
+    config = ConfigParser.RawConfigParser()
+    config.add_section('Database')
+    config.set('Database', 'filePath', r'C:\assetjet.db')
+    config.add_section('Updates')
+    config.set('Updates', 'url', r'www.assetjet.com/downloads')
+    
+    with open(os.path.join(location, fileName), 'w') as configfile:
+        config.write(configfile)
+
+
 class Cfg(ConfigParser.ConfigParser):
     DbFileName = ""
     UpdateUrl = ""
@@ -9,11 +25,10 @@ class Cfg(ConfigParser.ConfigParser):
         ConfigParser.ConfigParser.__init__(self)
         if cfgFile is None:
             return
-        defaultDbFileName = str(cfgFile.get('Database', 'filePath'))    
-        self.DbFileName = defaultDbFileName 
+        self.DbFileName = str(cfgFile.get('Database', 'filePath'))
         self.UpdateUrl = str(cfgFile.get('Updates', 'url'))    
         
 
 config = ConfigParser.ConfigParser()
-config.readfp(open(os.path.join(os.path.dirname(util.getBaseDir()), 'app.cfg')))
+config.readfp(open(os.path.join(location, fileName)))
 root = Cfg(config)

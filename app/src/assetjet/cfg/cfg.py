8b10ef0f9ca238c7e7c56b8b6b6f457b9301e966
Mel Padden <melpadden@gmail.com>
2013-01-10 19:05:25 +0100

    Merging

--- app/src/assetjet/cfg/cfg.py
+++ app/src/assetjet/cfg/cfg.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import ConfigParser, os
 from assetjet.util import util
@@ -32,3 +33,38 @@ class Cfg(ConfigParser.ConfigParser):
 config = ConfigParser.ConfigParser()
 config.readfp(open(os.path.join(location, fileName)))
 root = Cfg(config)
+=======
+
+import ConfigParser, os
+from assetjet.util import util
+
+class Cfg(ConfigParser.ConfigParser):
+    DbFileName = ""
+    UpdateUrl = ""
+    def __init__(self, cf=None):
+        ConfigParser.ConfigParser.__init__(self)
+        if cf is None:
+            return
+        if not cf.has_section('Database'):
+            cf.add_section('Database')
+            cf.set('Database', 'filePath', os.path.join(util.getBaseDir(), 'TimeSeries.db'))
+        defaultDbFileName = str(cf.get('Database', 'filePath'))    
+        self.DbFileName = defaultDbFileName 
+
+        if not cf.has_section('Updates'):
+            cf.add_section('Updates')
+            cf.set('Updates', 'url', 'http://assetjet.com/updates')
+        self.UpdateUrl = str(cf.get('Updates', 'url'))    
+        
+
+config = ConfigParser.ConfigParser()
+cfgPath = os.path.join(util.getBaseDir(), 'app.cfg')
+if not (os.path.isfile(cfgPath)):
+    f = open(cfgPath, 'w')
+    f.close()
+    
+    
+    
+config.readfp(open(cfgPath))
+root = Cfg(config)
+>>>>>>> Defaulty type logic
--- app/src/main.py
+++ app/src/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
     {copyright}
     AssetJet
@@ -39,4 +40,36 @@ if __name__ == '__main__':
 
     
     
+=======
+"""
+    {copyright}
+    AssetJet
+    Program Entry Point
+"""
+import os, sys
+from PySide import QtCore, QtGui
+from assetjet.log import log
+
+def main():
+    from assetjet.controller.main_controller import MainController 
+    from assetjet.cfg import cfg
+    from assetjet.util import updater
+        
+    upd = updater.Updater(cfg.root.UpdateUrl)
+    upd.setDaemon(daemonic=True)
+    upd.start()
+    
+    app = QtGui.QApplication(sys.argv)
+    mainForm = MainController()
+    mainForm.Show()
+    sys.exit(app.exec_())
+    sys.exit(0)
+
+if __name__ == '__main__':
+    log.Debug("Initialising app")
+    main()
+       
+    
+    
+>>>>>>> Defaulty type logic
     

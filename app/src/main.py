2edf719e85fa68b4eca531b090a6bfa4e3b161d3
Mel Padden <melpadden@gmail.com>
2013-01-10 19:08:01 +0100

    Re-Merged

--- app/src/main.py
+++ app/src/main.py
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-"""
-    {copyright}
-    AssetJet
-    Program Entry Point
-"""
-import os, sys
-from PySide import QtCore, QtGui
-from assetjet.log import log
-from assetjet.controller.main_controller import MainController 
-from assetjet.cfg import cfg
-from assetjet.util import updater
-
-def main():
-    upd = updater.Updater(cfg.root.UpdateUrl)
-    upd.daemon = True
-    upd.start()
-    
-    app = QtGui.QApplication(sys.argv)
-    mainForm = MainController()
-    mainForm.Show()
-    sys.exit(app.exec_())
-    sys.exit()
-
-if __name__ == '__main__':
-    if getattr(sys,"frozen",False):
-    # if app is a frozen executable, use logging
-        try:
-            log.Debug("Initialising app")
-            main()
-            sys.exit(0)
-           
-        except Exception, ex:
-            log.Error("Error in application: {0}", str(ex))
-            sys.exit(-1)
-    else:
-    # don't use logging for debugging
-        main()
-
-
-    
-    
-=======
 """
     {copyright}
     AssetJet
@@ -69,7 +26,4 @@ if __name__ == '__main__':
     log.Debug("Initialising app")
     main()
        
-    
-    
->>>>>>> Defaulty type logic
     

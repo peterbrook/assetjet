2069663ea4f0c3e7a4d1d045c9a8f3f54c5182a6
Felix Zumstein <fzumstein@gmail.com>
2013-01-12 00:18:19 +0100

    split up version string

--- app/src/assetjet/__init__.py
+++ app/src/assetjet/__init__.py
@@ -1,2 +1,8 @@
-__version__ = '0.1.6'
 __all__ = ["main", "cfg", "controller", "log", "model", "store", "view" ]
+
+# Version
+major = 0
+minor = 0
+micro = 6
+sub = 'dev'
+__version__ = "{0}.{1}.{2}{3}".format(major, minor, micro, sub)

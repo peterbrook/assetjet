90a1038f97d692efbf792b43ee825639fd56df2c
Felix Zumstein <fzumstein@gmail.com>
2013-02-24 19:04:50 +0100

    make update url dependend on dev/prod

--- app/src/assetjet/cfg/cfg.py
+++ app/src/assetjet/cfg/cfg.py
@@ -1,6 +1,12 @@
 import ConfigParser, os
 from assetjet.util import util
+import sys
 
+# appName will be AssetJet or AssetJetDev
+appName = os.path.splitext(os.path.split(sys.executable)[1])[0]
+
+# set name and location of config file. It has be outside of the src directory
+# as otherwise it would be wiped out upon every update (Esky)
 fileName = 'app.cfg'
 location = os.path.dirname(util.getBaseDir())
 
@@ -11,7 +17,10 @@ if not os.path.exists(os.path.join(location, fileName)):
     config.add_section('Database')
     config.set('Database', 'filePath', r'C:\assetjet.db')
     config.add_section('Updates')
-    config.set('Updates', 'url', r'http://www.assetjet.com/prod/downloads')
+    if appName == 'AssetJet':
+        config.set('Updates', 'url', r'http://www.assetjet.com/prod/downloads')
+    elif appName == 'AssetJetDev':
+        config.set('Updates', 'url', r'http://www.assetjet.com/dev/downloads')
     
     with open(os.path.join(location, fileName), 'w') as configfile:
         config.write(configfile)

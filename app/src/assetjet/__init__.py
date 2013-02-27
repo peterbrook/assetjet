bfa2bc125a90a3c4f7b0e2a3f2e2b06825581efd
Felix Zumstein <fzumstein@gmail.com>
2013-02-27 22:21:25 +0100

    brought into official python versioning style

--- app/src/assetjet/__init__.py
+++ app/src/assetjet/__init__.py
@@ -3,6 +3,6 @@ __all__ = ["main", "cfg", "controller", "log", "model", "store", "view", "servic
 # Version
 major = 0
 minor = 0
-micro = 6
-sub = 'dev'
-__version__ = "{0}.{1}.{2}.{3}".format(major, minor, micro, sub)
+micro = 10
+releaselevel = 'dev' # 'dev' or 'final'
+__version__ = "{0}.{1}.{2}{3}".format(major, minor, micro, releaselevel)

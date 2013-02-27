5f3edff2518e94ce21f79acaf5c68623b002e69d
Felix Zumstein <fzumstein@gmail.com>
2013-02-27 22:23:12 +0100

    extended for qrc resource files

--- sandbox/build_ui_views.py
+++ sandbox/build_ui_views.py
@@ -1,20 +1,40 @@
 import os
+from subprocess import call
 
-rootDir = 'C:\\src\\github\\assetjet'
-print ("Current path is: ", rootDir)
-outDir = os.path.join(rootDir, "tn\\view")
-outDir = 'C:\\src\\github\\assetjet\\app\\src\\assetjet\\view'
+# UIs
+
+rootDir = '..'
+targetDir = r'app/src/assetjet/view'
+
+outDir = os.path.join(rootDir, targetDir)
 fileList = []
 
 for root, subFolders, files in os.walk(rootDir):
-    for file in files:
-        if file.endswith(".ui"):
-            outFile = os.path.join(outDir, file.replace("ui", 'py'))
-            fileList.append((os.path.join(root,file), outFile))
+    for file_ in files:
+        if file_.endswith(".ui"):
+            outFile = os.path.join(outDir, file_.replace(".ui", '.py'))
+            fileList.append((os.path.join(root,file_), outFile))
 
 for uiFile, outFile in fileList:
     print "Building: {0} to: {1}".format(uiFile, outFile)
     cmd = "pyside-uic -o {0} {1}".format(outFile, uiFile)
-    os.system(cmd)
+    call(cmd)
 
-print "Done"
+print "UI files done"
+
+# Resources
+
+fileList = []
+
+for root, subFolders, files in os.walk(rootDir):
+    for file_ in files:
+        if file_.endswith(".qrc"):
+            outFile = os.path.join(outDir, file_.replace(".qrc", '_rc.py'))
+            fileList.append((os.path.join(root,file_), outFile))
+
+for rcFile, outFile in fileList:
+    print "Building: {0} to: {1}".format(rcFile, outFile)
+    cmd = "pyside-rcc -o {0} {1}".format(outFile, rcFile)
+    call(cmd)
+
+print "Rersource files done"

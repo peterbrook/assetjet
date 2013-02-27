531cb1e4e193b08c14ef0a9889d992931241a508
Felix Zumstein <fzumstein@gmail.com>
2013-02-27 22:22:56 +0100

    added icon and got rid of border around browser

--- app/src/assetjet/view/vw_main.py
+++ app/src/assetjet/view/vw_main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\src\github\assetjet\resources\ui\vw_main.ui'
+# Form implementation generated from reading ui file '..\resources\ui\vw_main.ui'
 #
-# Created: Wed Jan 16 13:26:31 2013
+# Created: Wed Feb 27 22:17:30 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,23 +12,32 @@ from PySide import QtCore, QtGui
 class Ui_Main(object):
     def setupUi(self, Main):
         Main.setObjectName("Main")
-        Main.resize(775, 589)
+        Main.resize(780, 589)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/Pie-chart.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Main.setWindowIcon(icon)
         self.centralwidget = QtGui.QWidget(Main)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_2 = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setSizeConstraint(QtGui.QLayout.SetDefaultConstraint)
         self.gridLayout.setObjectName("gridLayout")
         self.webView = QtWebKit.QWebView(self.centralwidget)
-        self.webView.setMinimumSize(QtCore.QSize(709, 519))
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.webView.sizePolicy().hasHeightForWidth())
+        self.webView.setSizePolicy(sizePolicy)
+        self.webView.setMinimumSize(QtCore.QSize(709, 528))
         self.webView.setUrl(QtCore.QUrl("http://www.google.ch/"))
         self.webView.setObjectName("webView")
         self.gridLayout.addWidget(self.webView, 0, 0, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         Main.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(Main)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 775, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 780, 21))
         self.menubar.setObjectName("menubar")
         Main.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(Main)
@@ -42,3 +51,4 @@ class Ui_Main(object):
         Main.setWindowTitle(QtGui.QApplication.translate("Main", "AssetJet", None, QtGui.QApplication.UnicodeUTF8))
 
 from PySide import QtWebKit
+import main_rc

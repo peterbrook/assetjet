2344e950f37701a22a0d50eab0b0eb76acb09f00
Mel Padden <melpadden@gmail.com>
2013-03-28 19:03:06 +0100

    Added WebSockets testing
    We want to move to a new communication layer. Testing shows that the
    WebSockets API is available in the PyQT browser.

--- app/src/httpdocs/js/aj.main.js
+++ app/src/httpdocs/js/aj.main.js
@@ -9,7 +9,27 @@
 		VERSION : "0.1"
 	,	rootUrl :"http://127.0.0.1:81"
 	};
-
+	AJ.webSocketTest = function()
+	{
+		if ("WebSocket" in window)
+		{
+			// Google example code
+			//  var ws = new WebSocket("ws://example.com/service");
+			//  ws.onopen = function()
+			//  {
+			//    // WebSocket is connected. You can send data by send() method
+			//    ws.send("message to send"); ....
+			//  };
+			//  ws.onmessage = function (evt) { var received_msg = evt.data; ... };
+			//  ws.onclose = function() { // websocket is closed. };
+			alert("WebSockets supported here!\r\n\r\nBrowser: " + navigator.userAgent + "\r\n\r\ntest by jimbergman.net (based on Google sample code)");
+	  	}
+	  	else
+	  	{
+	   		// the browser doesn't support WebSockets
+	    	alert("WebSockets NOT supported here!\r\n\r\nBrowser: " + navigator.userAgent + "\r\n\r\ntest by jimbergman.net (based on Google sample code)");
+	  	}
+	}
 	// String convenience functions
 	String.format = function() {
 		var s = arguments[0];

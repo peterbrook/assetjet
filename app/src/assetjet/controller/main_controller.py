af6c5acd97ce0ab79eb858a39176936881b8edd5
Felix Zumstein <fzumstein@gmail.com>
2013-04-22 22:33:38 +0200

    Cleaned up

--- app/src/assetjet/controller/main_controller.py
+++ app/src/assetjet/controller/main_controller.py
@@ -1,10 +1,7 @@
 import os
 from PySide import QtGui, QtCore
-import sqlalchemy.orm as orm
 from assetjet.view.vw_main import Ui_Main
-from assetjet.log import log
 from assetjet.util import util
-#from assetjet.model import asset
 from assetjet import __version__
 
 class MainController(QtGui.QMainWindow):
@@ -18,21 +15,10 @@ class MainController(QtGui.QMainWindow):
         self.ui = Ui_Main()
         self.ui.setupUi(self)
         mainPath = os.path.join(util.getBaseDir(), 'httpdocs', 'main.html')
-        print mainPath
         mainUrl = QtCore.QUrl.fromLocalFile(mainPath)
         self.ui.webView.load(mainUrl)
         self.ui.webView.url = mainUrl
         self.show()
         self.setWindowState(QtCore.Qt.WindowMaximized)
         self.setWindowTitle('AssetJet {0}'.format(__version__))
-
-        """
-        tickers = self.GetAllSymbols()
-
-        for i in range(0, len(tickers)):            
-            #liTicker = self.lstSymbols.model.item(i, 0)
-            liTicker = QtGui.QListWidgetItem(tickers[i].name)
-            self.ui.lstSymbolList.addItem(liTicker)
-        """
-if __name__ == "__main__":
-    print util.getBaseDir()
+        
